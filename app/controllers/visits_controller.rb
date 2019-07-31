a4fa95f20739111f90a071873ea2f02e30abd7d0
SkaMateria <skamateria@gmail.com>
2019-07-30 23:49:13 -0400

    added delete functionality to wishlist items

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -12,10 +12,13 @@ class VisitsController < ApplicationController
     end
 
     def destroy
+        #retrieves the currently logged in user's id and sets that to user_id
         user_id = current_user.id
+        #retrieves the museum's id chosen via the corresponding delete button and sets that to museum_id
         museum_id = params[:id].to_i
-        visit_id = current_user.visits.where(user_id: user_id, museum_id: museum_id)
-        visit_id.first.destroy
+        #searches the visits table for the correct visit that matches with the user id and museum id and sets it visit 
+        visit = current_user.visits.where(user_id: user_id, museum_id: museum_id).first
+        visit.destroy
         redirect_to user_path(current_user)
     end
 
