02b3593c77f60c1c141b895b3eee45c40bd06bcf
SkaMateria <skamateria@gmail.com>
2019-07-31 18:58:27 -0400

    refactoring CORRECTLY this time

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -10,21 +10,45 @@ class Museum < ApplicationRecord
         visited = self.visits.select { |visit| visit.visited == true }
         visited.map { |museum| museum.user }
     end
-
+    
+    # counts how many users have visited the museum
+    def visitors_count
+        self.visitors.count
+    end
+    
     # lists users who have wishlisted the museum
-    def wishlist
+    def wishlisted
         wishlisted = self.visits.select { |visit| visit.visited == false }
         wishlisted.map { |museum| museum.user }
     end
 
-    # counts how many users have visited the museum
-    def visitor_count
-        self.visitors.count
+    # counts how many users have wishlisted the museum
+    def wishlisted_count
+        self.wishlisted.count
+    end
+    
+    def not_visitors
+        User.all - self.users
     end
 
-    # counts how many users have wishlisted the museum
-    def wishlist_count
-        self.wishlist_count
+    def not_visitors_count
+        self.not_visitors.count
+    end
+
+    def self.visited_museums
+        Museum.all.map { |museum| museum.visits }.reject(&:empty?)
+    end
+
+    def self.visited_museums_count
+        Museum.visited_museums.count
+    end
+
+    def self.not_visited_museums
+        Museum.all.map { |museum| museum.visits }.select(&:empty?)
+    end
+
+    def self.not_visited_museums_count
+        Museum.not_visited_museums.count
     end
 
     #creates an array where the current museum is first 
@@ -36,17 +60,5 @@ class Museum < ApplicationRecord
         all_museum_visits += [museum_visits, other_museum_visits]
         all_museum_visits
     end 
-
-    def museum_list_by_wishlist
-
-    end
-
-    def museum_visits_by_date
-
-    end
-
-    # list comments
-
-    # count comments
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,44 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     has_secure_password
     
-    #list of museums wishlisted by current user
-    def wishlisted_museums
-        wishlist = self.visits.select { |visit| visit.visited == false }
-        wishlist.map { |visit| visit.museum }
-    end
-    
-    #list of museums actually visited by current user
+    # list of museums actually visited by current user
     def visited_museums
         visited = self.visits.select { |visit| visit.visited == true }
         visited.map { |visit| visit.museum }
     end
 
+    def visited_museums_count
+        self.visited_museums.count
+    end
+    
+    # list of museums wishlisted by current user
+    def wishlisted_museums
+        wishlist = self.visits.select { |visit| visit.visited == false }
+        wishlist.map { |visit| visit.museum }
+    end
+
+    def wishlisted_museums_count
+        self.wishlisted.count
+    end
+    
+    # list of museums you have not visited or wishlisted
+    def not_visited
+        Museum.all - self.wishlisted_museums - self.visited_museums
+    end
+
+    def not_visited_count
+        self.not_visited.count
+    end
+
+    # list of all comments starting from most recent
+    def comments_history
+        self.comments.map { |user| user.content }.reverse
+    end
+
+    def comments_history_count
+        self.comments_history.count
+    end
+    
     # lists all museums user has visited starting at the most recently visited
     def visited_by_date
         ordered_visits = self.visits.sort_by(&:updated_at).reverse
@@ -24,23 +50,7 @@ class User < ApplicationRecord
         ordered_visited.map { |visit| visit.museum }
     end
 
-    # creates an array = [museums visited, 
-    # number of museums put in wishlist, 
-    # number of museums not visited]
-
-    def not_visited
-        Museum.all - self.wishlisted_museums - self.visited_museums
-        # visited = self.visited_museums
-        # visited_museum_ids = visited.map { |museum| museum.id }
-        # wishlisted = self.wishlisted_museums
-        # not_visited = Museum.where.not(id: visited_museum_ids)
-        # objects_array = []
-        # count_array = []
-        # objects_array += [visited, wishlisted, not_visited] 
-        # count_array += [visited.count, wishlisted.count, not_visited.count]
-    end
  
-
     # creates array of all visits to museums 
     # by all users with the current user at the beginning
     def users_by_amount_visited
