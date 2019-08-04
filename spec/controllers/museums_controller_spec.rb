7692f338fc6de474e9ee1eb1c4aa0eddb2d982f7
Harum Helmy <harumendhah@gmail.com>
2019-08-04 19:09:12 -0400

    wrote two basic RSpec tests for museum and user show pages

---
+++ spec/controllers/museums_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe MuseumsController, type: :controller do
+
+    it "renders the show template" do
+        attributes =
+        {
+            name: "Museum of Stunning Paintings",
+            address: "1234 Best St NW"
+        } 
+        museum = Museum.create!(attributes)
+        get :show, params: { id: museum.id }
+    render_template(:show)
+    end
+
+
+end
+    
